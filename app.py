96eba29727801377dfed8de4deefe0b53568e8be
Martin Bächtold <martin@baechtold.me>
2024-10-06 16:35:00 +0200

    Add route to trigger download

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 import random
 
 from flask import Flask
 from flask import send_from_directory
 
 from config import IMAGES_PATH
+import flickr
 
 
 app = Flask(__name__)
@@ -15,6 +17,14 @@ def homepage():
     files = list(path.glob("*.jpg"))
     file = random.choice(files)
     return send_from_directory(path, file.name)
+
+
+@app.route("/download-images")
+def download_images():
+    if os.getenv("ENABLE_DOWNLOAD_ROUTE") != "1":
+        return "nope"
+    flickr.fetch()
+    return ""
 
 
 if __name__ == "__main__":
